ed43f93a359aa623dcc4bf83723a2b3b26b5b97f
codepoetpro <ignitor1996@gmail.com>
2019-01-09 02:05:07 +0100

    Created state.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from './components/Todo';
+import './App.css'
+
 
 class App extends Component {
+
+  state = {
+    todos : [
+      {
+        id : 1,
+        title: 'Take out the trash',
+        completed : false
+      },
+      {
+        id : 2,
+        title: 'Dinner with wife',
+        completed : false
+      },
+      {
+        id : 3,
+        title: 'Plan a meeting',
+        completed : false
+      }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Todos todos={this.state.todos}/>
       </div>
     );
   }
