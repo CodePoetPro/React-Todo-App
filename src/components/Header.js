48a55d31af4240f36466d3e2030ae437c93e7665
codepoetpro <ignitor1996@gmail.com>
2019-01-13 19:49:46 +0100

    Added links to pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
+
 
 export default function Header() {
   return (
     <header>
       <h4>React TodoList</h4>
+      <Link to="/">Home</Link> |
+      <Link to="/about">About</Link>
     </header>
   )
 }
